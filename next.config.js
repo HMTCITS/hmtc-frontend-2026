1244c5dbfb9835a13ee78440b494d300e070024f
Raden Kurniawan Agung Fitrianto <rkurniawanagung32@gmail.com>
2026-04-14 13:33:48 +0700

    feat: migrate from webpack to turbopack

--- next.config.js
+++ next.config.js
@@ -61,43 +61,26 @@ const nextConfig = {
     return [];
   },
 
-  webpack(config, { isServer }) {
-    // Exclude .svg dari rule bawaan
-    const assetRule = config.module.rules.find(
-      (rule) => rule.test instanceof RegExp && rule.test.test('.svg'),
-    );
-    if (assetRule) {
-      assetRule.exclude = /\.svg$/i;
-    }
-    // SVGR loader
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: { and: [/\.[jt]sx?$/] },
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            prettier: false,
-            svgo: true,
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-            titleProp: true,
-            ref: true,
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              prettier: false,
+              svgo: true,
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+              titleProp: true,
+              ref: true,
+            },
           },
-        },
-      ],
-    });
-    // Suppress dynamic require warnings from Sentry / otel libs in client bundle by aliasing to empty module
-    if (!config.resolve) config.resolve = {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      ...(!isServer && {
-        '@sentry/node': false,
-        '@sentry/nextjs': false,
-        'require-in-the-middle': false,
-        '@opentelemetry/instrumentation': false,
-      }),
-    };
-    return config;
+        ],
+        as: '*.js',
+      },
+    },
   },
 
   images: {
